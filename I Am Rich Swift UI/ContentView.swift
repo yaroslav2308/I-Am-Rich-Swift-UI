d7139364c9e2ab185db8c3985eb9972383809a17
yaroslav2308 <yaroslav.monastyrev@gmail.com>
2021-11-11 21:16:03 +0300

    Initial and final commit

--- I Am Rich Swift UI/ContentView.swift
+++ I Am Rich Swift UI/ContentView.swift
@@ -9,8 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 1, green: 0, blue: 1)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("I Am Rich")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: .center)
+            }
+        }
     }
 }
 
