6b8001d04774d7e4d1a8876e8393c49266706664
yaroslav2308 <yaroslav.monastyrev@gmail.com>
2021-11-11 20:46:38 +0300

    Initial Commit

---
+++ I Am Rich Swift UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I Am Rich Swift UI
+//
+//  Created by Yaroslav Monastyrev on 11.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ I Am Rich Swift UI/I_Am_Rich_Swift_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_Rich_Swift_UIApp.swift
+//  I Am Rich Swift UI
+//
+//  Created by Yaroslav Monastyrev on 11.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_Rich_Swift_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
